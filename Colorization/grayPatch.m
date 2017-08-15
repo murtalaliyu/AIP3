ea69ce517ab2b9e88507e18fba7bc87d26fab75b
murtalaliyu <murtalaliyu10@yahoo.com>
2017-08-14 21:50:19 -0400

    added color shades

--- Colorization/grayPatch.m
+++ Colorization/grayPatch.m
@@ -1,15 +1,17 @@
 %takes an mx9 input and returns an mx3 matrix of rgb values
-function [mat] = grayPatch(input)
+function [shade] = grayPatch(input)
     [x,~] = size(input);
-    mat = zeros(x,3);
+    shade = zeros(x,1);
     for i=1:x
         average = round(sum(input(i,:)) / 9);
         
-        A = [.21 0 0; 0 .72 0; 0 0 .07];
-        B = round([.21*average; .72*average; .07*average]);
-        X = round(A\B)';
-        mat(i,1) = X(1,1);
-        mat(i,2) = X(1,2);
-        mat(i,3) = X(1,3);
+        if (average < 100) %a dark shade
+            shade(i,:) = 1;
+        elseif (average >= 200) %a light shade
+            shade(i,:) = 2;
+        else %just a normal (neutral) shade
+            shade(i,:) = 3;
+        end
+        
     end
 end
